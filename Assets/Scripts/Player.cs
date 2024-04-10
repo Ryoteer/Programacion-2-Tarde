8105dd92461aabd421b85c281f85389948ea4853
Ryoteer <72623439+Ryoteer@users.noreply.github.com>
2024-04-10 17:26:12 -0300

    Added Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,14 +8,23 @@ public class Player : MonoBehaviour
     [Header("Animator")]
     [SerializeField] private string _attackTriggerName = "onAttack";
     [SerializeField] private string _moveBoolName = "isMoving";
+    [SerializeField] private string _jumpStartName = "onJump";
+    [SerializeField] private string _landingName = "onLanding";
     [SerializeField] private string _xAxisName = "xAxis";
     [SerializeField] private string _zAxisName = "zAxis";
 
+    [Header("Inputs")]
+    [SerializeField] private KeyCode _attackKey = KeyCode.Mouse0;
+    [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
+
     [Header("Values")]
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private float _speed = 5f;
 
     private float _xAxis, _zAxis;
     private Vector3 _dir = new Vector3();
+    private bool _canJump = true;
 
     private Animator _animator;
     private Rigidbody _rb;
@@ -40,7 +49,7 @@ public class Player : MonoBehaviour
         _animator.SetFloat(_xAxisName, _xAxis);
         _animator.SetFloat(_zAxisName, _zAxis);
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(_attackKey))
         {
             _animator.SetTrigger(_attackTriggerName);
 
@@ -53,6 +62,11 @@ public class Player : MonoBehaviour
                 _animator.SetBool(_moveBoolName, false);
             }
         }
+
+        if (Input.GetKeyDown(_jumpKey) && _canJump)
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
@@ -63,10 +77,29 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Jump()
+    {
+        _canJump = !_canJump;
+
+        _animator.SetTrigger(_jumpStartName);
+
+        _rb.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
     private void Movement(float xAxis, float zAxis)
     {
         _dir = (transform.right * xAxis + transform.forward * zAxis).normalized;
 
         _rb.MovePosition(transform.position + _dir * _speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 9)
+        {
+            _canJump = true;
+
+            _animator.SetTrigger(_landingName);
+        }
     }
 }
